46da95c40153566f478b7f5d867b3726b904ad59
iyeonggyu0 <iyeonggyu0@gmail.com>
2025-09-13 00:04:14 +0900

    어드민 로그인 페이지 및 공용 CP 제작

--- src/App.jsx
+++ src/App.jsx
@@ -24,9 +24,38 @@ function App() {
         {/* 문의하기 */}
         <Route path="/kakao" element={<KakaoPage />} />
 
+        {/* LogIn */}
+        <Route path="/login" element={<MainPage />} />
+
         {/* SignUp */}
+        {/* 가입 인사 */}
         <Route path="/signup/1" element={<HelloPage />} />
+        {/* 사용자 정보 입력 */}
         <Route path="/signup/2" element={<InfoPage />} />
+        {/* 카테고리 선택 */}
+        <Route path="/signup/3" element={<InfoPage />} />
+        {/* 카테고리별 키워드 설정 */}
+        <Route path="/signup/4" element={<InfoPage />} />
+        {/* 번호 인증 */}
+        <Route path="/signup/5" element={<InfoPage />} />
+        {/* 어플 설치 안내 */}
+        <Route path="/signup/6" element={<InfoPage />} />
+        {/* 가입 완료 */}
+        <Route path="/signup/7" element={<InfoPage />} />
+
+        {/* 마이페이지 */}
+        <Route path="/mypage/info" element={<InfoPage />} />
+        <Route path="/mypage/notice" element={<InfoPage />} />
+
+        {/* 기기 등록 - 해당 페이지는 메인화면 연결, 추가 등록은 마이페이지에서 */}
+        <Route path="/append" element={<InfoPage />} />
+
+        {/* 탈퇴 */}
+        <Route path="/withdraw" element={<InfoPage />} />
+
+        {/* 공지사항 */}
+        <Route path="/notice" element={<InfoPage />} />
+        <Route path="/notice/:id" element={<InfoPage />} />
 
         {/* 404에러 */}
         <Route path="*" element={<Error404Page />} />
--- src/layout/Header/index.jsx
+++ src/layout/Header/index.jsx
@@ -4,10 +4,13 @@ import LogoCP from "../../component/_common/logoCP";
 import { useWeb } from "../../hook/useWeb";
 import "./style.css";
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
   const [isLogin, setIsLogin] = useState(null);
   const isApp = useWeb().isApp;
+
+  const nav = useNavigate();
 
   useEffect(() => {
     async function checkLogin() {
@@ -21,9 +24,10 @@ const Header = () => {
     <header className="MainPageLayoutHeader flexBetween">
       <LogoCP />
       <div className="MainPageLayoutHeader-content flexBetween">
-        {isApp && <p>기기 등록</p>}
-        {!isLogin && <p>로그인</p>}
-        {isLogin && <p>마이페이지</p>}
+        <p onClick={() => nav("/")}>공지 보기</p>
+        {isApp && <p onClick={() => nav("/")}>기기 등록</p>}
+        {!isLogin && <p onClick={() => nav("/")}>로그인</p>}
+        {isLogin && <p onClick={() => nav("/")}>마이페이지</p>}
       </div>
     </header>
   );
