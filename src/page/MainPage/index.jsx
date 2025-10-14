0ee42b13299a394b018ec9b1137b3721549897d9
iyeonggyu0 <iyeonggyu0@gmail.com>
2025-10-14 23:24:43 +0900

    회원가입 버튼 추가

--- src/page/MainPage/index.jsx
+++ src/page/MainPage/index.jsx
@@ -2,14 +2,14 @@ import { useNavigate } from "react-router-dom";
 import MainLayout from "../../layout";
 import "./style.css";
 
-const MainPage = (login) => {
+const MainPage = () => {
   const nav = useNavigate();
 
   return (
     <MainLayout>
       <section className="mainPage">
         <div>{/* 이미지 */}</div>
-        {!login && <p onClick={() => nav("/signup/1")}>회원가입</p>}
+        <p onClick={() => nav("/signup/1")}>회원가입</p>
       </section>
     </MainLayout>
   );
