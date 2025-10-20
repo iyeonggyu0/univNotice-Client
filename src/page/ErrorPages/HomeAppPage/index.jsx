fa4d4117f866db4044aeb8ace92978fb153d3597
iyeonggyu0 <iyeonggyu0@gmail.com>
2025-10-20 22:16:28 +0900

    로딩 기능 추가

--- src/page/ErrorPages/HomeAppPage/index.jsx
+++ src/page/ErrorPages/HomeAppPage/index.jsx
@@ -1,6 +1,11 @@
+import MainLayout from "../../../layout";
 import "./style.css";
 
 const HomeAppPage = () => {
-  return;
+  return (
+    <MainLayout>
+      <div>해당 univNotice 서비스는, iOS 16.4 이상 버전에서 사용 가능합니다. (지원 대상 기종은 아이폰 8 이상 모델)</div>
+    </MainLayout>
+  );
 };
 export default HomeAppPage;
--- src/page/MainPage/index.jsx
+++ src/page/MainPage/index.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import MainLayout from "../../layout";
 import "./style.css";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const MainPage = ({ mainPageLayout }) => {
   const nav = useNavigate();
@@ -9,7 +10,8 @@ const MainPage = ({ mainPageLayout }) => {
     <MainLayout mainPageLayout={mainPageLayout}>
       <section className="mainPage">
         <div>{/* 이미지 */}</div>
-        <p onClick={() => nav("/signup/1")}>회원가입</p>
+        {mainPageLayout && <p onClick={() => nav("/signup/1")}>회원가입</p>}
+        {!mainPageLayout && <CircularProgress size="24px" />}
       </section>
     </MainLayout>
   );
