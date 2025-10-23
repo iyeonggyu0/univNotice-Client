ceee54068c0a80cf44b2bf356a3a39f7fb61bf01
iyeonggyu0 <iyeonggyu0@gmail.com>
2025-10-23 20:48:41 +0900

    로그인 로직 표시 수정

--- src/page/MainPage/index.jsx
+++ src/page/MainPage/index.jsx
@@ -11,7 +11,7 @@ const MainPage = ({ mainPageLayout, login }) => {
       <section className="mainPage">
         <div>{/* 이미지 */}</div>
         {mainPageLayout && !login && <p onClick={() => nav("/signup/1")}>회원가입</p>}
-        {mainPageLayout && !login && <p onClick={() => nav("/signup/1")}>회원가입</p>}
+        {mainPageLayout && login && <p onClick={() => nav("/notice")}>공지 보기</p>}
         {!mainPageLayout && <CircularProgress color="inherit" className="loading" size="22px" />}
       </section>
     </MainLayout>
