57dfeaf87b8edff8ab66f3493b12f6be7c1ca1bd
iyeonggyu0 <iyeonggyu0@gmail.com>
2025-11-16 19:31:25 +0900

    편의성 증대

--- src/page/SignUpPage/1_PhoneSelectPage/index.jsx
+++ src/page/SignUpPage/1_PhoneSelectPage/index.jsx
@@ -20,13 +20,9 @@ const PhoneSelectPage = () => {
         <div className="centerBox">
           <div className="titleBox">
             <h2 className="title">
-              <span className="bold">핸드폰 기종</span>을 선택해 주세요
+              <span className="bold">기종</span>을 선택해 주세요
             </h2>
-            <h4 className="subTitle">
-              아이폰과 안드로이드는 서로 다른 방식으로 알림을 수신해요
-              <br />
-              잘못 선택 시, 알림이 오지 않을 수 있어요
-            </h4>
+            <h4 className="subTitle">잘못 선택 시, 알림이 오지 않을 수 있어요</h4>
           </div>
           <div className="flexCol">
             <div
